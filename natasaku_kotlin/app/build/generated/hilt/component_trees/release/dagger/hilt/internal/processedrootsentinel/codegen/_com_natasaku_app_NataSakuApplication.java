7eae474840ebcce3f4a1d35ed0141bfed79d8845
NataSaku Dev <dev@natasaku.com>
2026-05-23 19:36:55 +0700

    chore: stop tracking build artifacts

--- natasaku_kotlin/app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_natasaku_app_NataSakuApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.natasaku.app.NataSakuApplication"
-)
-public final class _com_natasaku_app_NataSakuApplication {
-}
